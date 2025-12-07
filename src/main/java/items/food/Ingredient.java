f4626bccfba77d46134823ed82910965aa5f4188
astebern <satriaguna06@gmail.com>
2025-12-07 16:27:54 +0700

    feat: Ingredient

--- src/main/java/items/food/Ingredient.java
+++ src/main/java/items/food/Ingredient.java
@@ -1,6 +1,59 @@
 package items.food;
 
-public class Ingredient {
-    
-}
+import items.Item;
+import items.Preparable;
 
+public class Ingredient extends Item implements Preparable {
+    private IngredientState state;
+
+    public Ingredient(String name) {
+        super(name);
+        this.state = IngredientState.RAW;
+    }
+
+    public IngredientState getState() {
+        return state;
+    }
+
+    public void setState(IngredientState state) {
+        this.state = state;
+    }
+
+    @Override
+    public boolean canBeChopped() {
+        return this.state == IngredientState.RAW;
+    }
+
+    @Override
+    public boolean canBeCooked() {
+        return this.state == IngredientState.RAW || this.state == IngredientState.CHOPPED;
+    }
+
+    @Override
+    public boolean canBePlacedOnPlate() {
+        return this.state != IngredientState.BURNED;
+    }
+
+    @Override
+    public void chop() {
+        if (canBeChopped()) {
+            this.state = IngredientState.CHOPPED;
+            System.out.println(name + " dipotong menjadi CHOPPED.");
+        }
+    }
+
+    @Override
+    public void cook() {
+        this.state = IngredientState.COOKED;
+    }
+    
+    // Method helper untuk logic hangus
+    public void burn() {
+        this.state = IngredientState.BURNED;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (" + state + ")";
+    }
+}
